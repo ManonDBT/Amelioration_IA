193a147d6cec915fa8059d1c51beeee522dbc144
Manon <manondbt@live.fr>
2021-04-29 11:50:01 +0200

    dash

--- Amélioration/Modèle/Amelioration_scoring.py
+++ Amélioration/Modèle/Amelioration_scoring.py
@@ -37,7 +37,7 @@ for i in range(len(X)):
 for i in range(len(y)):
     y[i] = int(float(y[i])*10)
     
-X_train, X_test, y_train, y_test = train_test_split (X,y, test_size = 0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state=42)
 
 model = tree.DecisionTreeClassifier()
 model.fit(X_train, y_train)
